d6e25c157d0eeed1d82eacd62d804cb25311ff58
Matthieu <matthieu.clio@gmail.com>
2023-07-10 16:59:40 +0200

    Fix : end git issue.

--- js/app.js
+++ js/app.js
@@ -2,19 +2,15 @@
 
 class App {
     constructor () {
-    constructor () {
         // Data
         this.data = recipes // ./js/data/recipes/js - const recipes
         
-        
         // Component PrimarySearch
-        this.primarySearch = new PrimarySearch(this.data)
         this.primarySearch = new PrimarySearch(this.data)
     }
 
     mainSearch () {
         this.primarySearch.handleChange()
-        this.primarySearch.tagChange()
         this.primarySearch.tagChange()
     }
 }
--- js/components/primarySearch.js
+++ js/components/primarySearch.js
@@ -1,5 +1,9 @@
 class PrimarySearch {
-    constructor() {
+    constructor (data) {
+        // Data
+        this.data = data
+        this.matchData = []
+
         // Dom elements
         this.rowReceipt = document.getElementById('row-receipts')
         this.searchInputText = document.getElementById('search-input-text')
@@ -9,20 +13,78 @@ class PrimarySearch {
         this.dataFactory = new DataFactory()
     }
 
-    default(data) {
+    run (data) {
         this.dataFactory.display(data)
     }
 
-    handleChange() {
-        this.searchInputText.addEventListener('keyup', (event) => {
+    handleChange () {
+        // Display all receipts
+        this.run(this.data)
+
+        this.searchInputText.addEventListener('keyup', () => {
+
             // Start the search after 3 characters
-            if(this.searchInputText.value.length > 2) {
-                console.log(this.searchInputText.value)
+            if (this.searchInputText.value.length > 2) {
+
+                // Erase the previous search
+                this.reinitialized()
+
+                // Check the correspondence of the name, ingredients and description compared to the data
+                this.data.forEach((element) => {
+
+                    const rule = this.searchInputText.value.toLowerCase()
+                    const regex = new RegExp(rule, 'gm')
+
+                    // Regex for name
+                    const checkDataName = element.name.toLowerCase()
+                    const nameResult = checkDataName.match(regex)
+                    // console.log(nameResult)
+
+                    // Regex for description
+                    const checkDataDescription = element.description.toLowerCase()
+                    const descriptionResult = checkDataDescription.match(regex)
+                    // console.log(descriptionResult)
+
+                    // If the name matches we push the result
+                    if (nameResult !== null) {
+                        console.log(checkDataName)
+                        this.matchData.push(element)
+
+                    // If the description matches we push the result
+                    } else if (descriptionResult !== null) {
+                        console.log(checkDataDescription)
+                        this.matchData.push(element)
+                    }
+
+                    // Stock data specific element
+                    const dataElement = element
+
+                    element.ingredients.forEach((element) => {
+
+                        // Regex for ingredients
+                        const checkDataIngredients = element.ingredient.toLowerCase()
+                        const ingredientsResult = checkDataIngredients.match(regex)
+
+                        // If the ingredients matches we push the result if isn't already pushed
+                        if (nameResult === null && descriptionResult === null && ingredientsResult !== null) {
+                            console.log(checkDataIngredients)
+                            this.matchData.push(dataElement)
+                        }
+                    })
+                })
+                // Display the new search
+                this.run(this.matchData)
+
+                // Reset the result for the new search
+                this.matchData = []
+
+            // make a default search if input is empty
+            } else if (this.searchInputText.value === '') {
+                this.run(this.data)
             }
-            // var name = event.key;
-            // var code = event.code;
-            // console.log(name)
-            // console.log(code)
+
+        }) //End event keyup
+    }
 
     tagChange() {
         this.chevronDown.addEventListener('click', () => {
@@ -30,9 +92,7 @@ class PrimarySearch {
         })
     }
 
-    change() {
-        // this.searchInputText.addEventListener('change', (event) => {
-        //     console.log('change')
-        // })
+    reinitialized() {
+        this.rowReceipt.innerHTML = ""
     }
 }
